da9ec2a102aae2cefe6b94e9988bc22a7e9c09c6
Margaret Caroline Steiner <steinerm@midway2-login1.rcc.local>
2023-11-27 13:29:34 -0600

    update scripts

---
+++ empirical/subsampling/run_snakemake.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#SBATCH -J ukb_sfs
+#SBATCH --account=pi-jnovembre
+#SBATCH --mail-type ALL
+#SBATCH --mail-user=steinerm@uchicago.edu
+#SBATCH --partition=broadwl
+#SBATCH --time=08:00:00
+
+module load python
+module load R
+source /software/python-anaconda-2020.02-el7-x86_64/etc/profile.d/conda.sh
+conda activate snakemake
+
+snakemake --profile slurm --ignore-incomplete 
+   
+
