e7ce80cc87cc5d6a6f2eee72ec24a5b8ba9dc42b
Maggie Steiner <steinerm@uchicago.edu>
2024-02-23 10:19:29 -0600

    Updates to code (#1)
    * update code
    
    Made first round of edits to code. Main change was changing data structure to only store locations and marking as nan when particle dies. Also added in several smaller fixes as we discussed (e.g. update x and y coordinates at same time, switch from while loop to for loop...).
    
    Changes still remaining to make:
    
     Move to new API for np.random
     Replace the string labels for events to enum class
     Add unit tests
     Write function to normalize over # samples & implement binomial sampling to get finite sample SFS
     (Later) implement spatial sampling
    
    * response to PR comments
    
    Response to @dp-rice suggestions - mostly style edits

--- bp_sims/source/simulations.py
+++ bp_sims/source/simulations.py
@@ -1,76 +1,110 @@
 import numpy as np
 from numpy.random import exponential
-import pandas as pd
 import argparse
 
-def time_to_next(k,s,mu,rho,r):
-    return exponential(k*(1-s)+k+mu*rho+r)
+def time_to_next(k,s,theta,r):
+    return exponential(k*(1-s)+k+theta+r)
 
-def choose_event(k,s,mu,rho,r):
-    tot = k*(1-s)+k+mu*rho+r
-    event = np.random.choice(['b','d','m','s'],p=[(k*(1-s)/tot),(k/tot),(mu*rho/tot),(r/tot)])
+def choose_event(k,s,theta,r):
+    tot = k*(1-s)+k+theta+r
+    event = np.random.choice(['b','d','m','s'],p=[(k*(1-s)/tot),(k/tot),(theta/tot),(r/tot)])
     return event
 
+def get_alive(locations):
+    return np.where(~np.isnan(locations[:, 0]))[0]
+
+def get_nan(locations):
+    return np.where(np.isnan(locations[:, 0]))[0]
+
+def wrap_locations(locations,L):
+    return locations % L
+
+def extend_locations(locations):
+    return np.pad(locations, pad_width=((0, locations.shape[0]), (0, 0)), constant_values=np.nan)
+
+def update_locations(locations,sigma,t_next,L):
+    alive_rows = get_alive(locations)
+    if len(alive_rows) > 0:
+        locations[alive_rows] += np.random.normal(loc=0, scale=sigma * np.sqrt(t_next), size=(len(alive_rows), 2))
+    locations = wrap_locations(locations, L)
+    return locations
 
 def run_sim_spatial(s, mu, rho, r, sigma, num_iter, max_ind, L=50, sfs_len=100):
+    """
+    * Carriers appear de novo with rate `mu`*`rho`
+    * Carriers give birth (split) with rate `1-s`
+    * Carriers die at rate `1`
+    * Take samples at rate `r`
+    * Habitat is a square (0,L)x(0,L) with periodic boundary conditions
+
+    Implement via Gillespie algorithm. At each step:
+    1. Draw waiting time to next event
+    2. Update locations of living carriers
+    3. Select event type and apply it
+
+    Data structures:
+    * During simulation, store locations of individuals at the current time
+    * At each sample point update the SFS distribution (array w/ pre-specified length)
+    * Output SFS distribution
+    """
+
+    # parameter for total mutation rate
+    theta = mu*rho*(L**2)
+
     # initialize array for SFS distribution
     sfs_dist = np.zeros(sfs_len)
     # keep track of time steps
-    counter = 1  # initialize at 1
+
     # keep track of individual level data
-    # [alive/dead, x coord, y coord, time @ birth, time @ death]
-    ind_data = np.zeros((max_ind, 5))
+    # [x coord, y coord]
+    locations = np.full((max_ind, 2),np.nan)
     # initialize current time at 0
-    curr_time = 0
-    while counter <= num_iter:
-        k = np.sum(ind_data[:, 0])  # number of alive particles
+    for _ in range(num_iter):
+        alive_rows = get_alive(locations)
+        k = len(alive_rows)  # number of alive particles
         # draw time to next event
-        t_next = time_to_next(k, s, mu, rho, r)
-        # update time
-        curr_time += t_next
+        t_next = time_to_next(k,s,theta,r)
         # draw event type
-        e_type = choose_event(k, s, mu, rho, r)
+        e_type = choose_event(k,s,theta,r)
 
         ### update spatial coordinates
-        indices = np.where(ind_data[:, 0] == 1)[0]
-        if len(indices) > 0:
-            ind_data[indices, 1] += np.random.normal(loc=0, scale=0.2 * t_next, size=len(indices))  # update x
-            ind_data[indices, 2] += np.random.normal(loc=0, scale=0.2 * t_next, size=len(indices))  # update y
+        locations = update_locations(locations,sigma,t_next,L)
 
         ### mutation
         if e_type == 'm':
             # find next empty row
-            empty_row_indices = np.where(np.all(ind_data == 0, axis=1))[0]
+            empty_row_indices = get_nan(locations)
             if len(empty_row_indices) > 0:  # check that there is a row available
                 next_row = empty_row_indices[0]  # choose the first available row
             else:
-                print("ERROR: ran out of room in array!")  # print error message and stop
-                break
+                print("Doubling array size")
+                next_row = locations.shape[0] + 1
+                locations = extend_locations(locations)
+
             # add row for new lineage at random location
-            ind_data[next_row, :] = [1, np.random.uniform(0, L), np.random.uniform(0, L), curr_time, 0]
+            locations[next_row] = np.random.uniform(0, L, size=2)
 
         ### death
         elif e_type == 'd':
             ## choose individual who dies
-            indices = np.where(ind_data[:, 0] == 1)  # all alive individuals
-            random_index = np.random.choice(indices[0])  # choose one at random
-            ind_data[random_index, 0] = 0  # mark first column to dead
-            ind_data[random_index, 4] = curr_time  # mark time of death
+            random_index = np.random.choice(alive_rows)  # choose one at random
+            locations[random_index] = np.nan  # mark coords to nan
 
         ### birth
         elif e_type == 'b':
             # find next empty row
-            empty_row_indices = np.where(np.all(ind_data == 0, axis=1))[0]
+            empty_row_indices = get_nan(locations)
             if len(empty_row_indices) > 0:  # check that there is a row available
                 next_row = empty_row_indices[0]  # choose the first available row
             else:
-                print("ERROR: ran out of room in array!")  # print error message and stop
-                break
+                print("Doubling array size")
+                next_row = locations.shape[0] + 1
+                locations = extend_locations(locations)
+
             # choose parent at random from alive individuals
-            indices = np.where(ind_data[:, 0] == 1)
-            random_index = np.random.choice(indices[0])
+            parent_index = np.random.choice(alive_rows)
             # add row for new (split) lineage with location of parent
-            ind_data[next_row, :] = [1, ind_data[random_index, 1], ind_data[random_index, 2], curr_time, 0]
+            locations[next_row] = locations[parent_index]
 
         ### sample NOTE: WILL WANT TO UPDATE TO SPATIAL SAMPLING
         ### currently counts number of extant lineages & updates SFS
@@ -81,12 +115,11 @@ def run_sim_spatial(s, mu, rho, r, sigma, num_iter, max_ind, L=50, sfs_len=100):
                 if k > 0:
                     print(k)
             else:
-                print("Error: SFS entry out of bounds")
+                print("Error: SFS entry out of bounds ("+str(k)+")")
 
-        # update counter
-        counter += 1
 
-    return sfs_dist, ind_data
+
+    return sfs_dist, locations
 
 def main():
     parser = argparse.ArgumentParser()
@@ -99,11 +132,22 @@ def main():
     parser.add_argument('--max_ind',type=int,help='max number of individuals',default=1000)
     parser.add_argument('-L',type=float,help='habitat width',default=50)
     parser.add_argument('--sfs_length',type=int,help='max length of sfs to output',default=1000)
+    parser.add_argument('--sfs_out',type=str,help='output file name for sfs',default='sfs.csv')
+    parser.add_argument('--loc_out',type=str,help='output file name for locations',default='loc.csv')
+    parser.add_argument('--seed',type=int,help='random string',default=2024)
     args = parser.parse_args()
 
+    # set seed
+    np.random.seed(args.seed)
+
+    # run simulation
     counts, df = run_sim_spatial(s=args.s, mu=args.mu, rho=args.dens, r=args.r, sigma=args.sigma, num_iter=args.num_iter,
                                  max_ind=args.max_ind, L=args.L, sfs_len=args.sfs_length)
-    print(counts[0:10])
+
+    # save output as CSV
+    np.savetxt(args.sfs_out,counts,delimiter=',')
+    np.savetxt(args.loc_out, df, delimiter=',')
+
 if __name__ == '__main__':
     main()
 
