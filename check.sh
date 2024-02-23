bf405b56a7f62e338e3f77366df4ecefd88a213b
Daniel P. Rice <daniel.paul.rice@gmail.com>
2024-02-23 14:36:59 -0500

    Run mypy to check types

--- check.sh
+++ check.sh
@@ -5,3 +5,9 @@ if ! ./bp_sims/source/test.py; then
     echo "FAIL: tests"
     exit 1
 fi
+
+echo Running mypy to check types...
+if ! mypy --pretty bp_sims/source/; then
+    echo "FAIL: types"
+    exit 1
+fi
