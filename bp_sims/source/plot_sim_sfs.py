0f34ec04903a8ce9cb8218aba3b62fddbd0ce0ed
Margaret Caroline Steiner <steinerm@midway2-login1.rcc.local>
2024-02-29 09:31:08 -0600

    add new plots with bug fix

--- bp_sims/source/plot_sim_sfs.py
+++ bp_sims/source/plot_sim_sfs.py
@@ -15,7 +15,7 @@ def finite_sfs_k_unif(k,n,s,mu,N):
 
 def plot_sim_sfs(input_file,output_file,plot_theory,n,s,mu,N,max_x=501):
     df = pd.read_csv(input_file,header=None)
-    print(df)
+    #print(df)
     fig, axs = plt.subplots(1,1,figsize=(8,8))
     axs.loglog(np.arange(0,max_x),df[0:max_x],label='simulation',marker='o')
     axs.set_ylim(1e-8,1e-1)
