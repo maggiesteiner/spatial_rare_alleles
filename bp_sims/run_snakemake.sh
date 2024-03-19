60545e999fda1981e47935476f25eb8861f5371e
Margaret Caroline Steiner <steinerm@midway2-login1.rcc.local>
2024-03-19 17:30:34 -0500

    add updated results

--- bp_sims/run_snakemake.sh
+++ bp_sims/run_snakemake.sh
@@ -2,7 +2,7 @@
 #SBATCH -J bp_sims
 #SBATCH --account=pi-jnovembre
 #SBATCH --partition=broadwl
-#SBATCH --time=08:00:00
+#SBATCH --time=36:00:00
 
 module load python
 source /software/python-anaconda-2020.02-el7-x86_64/etc/profile.d/conda.sh
