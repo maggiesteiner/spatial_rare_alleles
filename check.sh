b2f437351f148fa4164662d8509e117bc83d5b3d
dp-rice <daniel.paul.rice@gmail.com>
2024-05-15 11:16:02 -0400

    Remove max_ind argument (#43)

--- check.sh
+++ check.sh
@@ -13,7 +13,7 @@ if ! mypy --pretty bp_sims/source/; then
 fi
 
 echo Fuzzing simulations.py...
-if ! python bp_sims/source/simulations.py --json_out /dev/null -s 0.1 -L 50 --sampling_scheme "uniform" --mu 1e-08 --dens 1 --max_ind 10 --n_side 1 --time_limit 10 -r 0.1; then
+if ! python bp_sims/source/simulations.py --json_out /dev/null -s 0.1 -L 50 --sampling_scheme "uniform" --mu 1e-08 --dens 1 --n_side 1 --time_limit 10 -r 0.1; then
     echo "FAIL: simulations.py"
     exit 1
 fi
