84c4ddb13772f3e9004105c872b4469f1d14f757
Mariadaria Kathrine Ianni-Ravn <mianniravn@midway2-login1.rcc.local>
2024-07-26 16:37:38 -0500

    reorganise

--- simulations/scripts/compile_sfs.R
+++ simulations/scripts/compile_sfs.R
@@ -18,7 +18,7 @@ dfs <- list()
 for (file in file_list) {
   # Extract the information from the file name
   file_name <- basename(file)
-  info <- strsplit(file_name, "_", fixed = TRUE)[[1]] %>% extract_numeric() 
+  info <- strsplit(file_name, "_", fixed = TRUE)[[1]] %>% parse_number()
   info <- info[is.na(info)==F]
   file <- paste0(inputdir,"/",file)
   # Read the file, skipping the first row (commented out line)
@@ -27,7 +27,7 @@ for (file in file_list) {
   # Extract the metadata from the first line
   metadata <- readLines(file, n = 1)
   num_inds_gt_1 <- as.numeric(str_extract(metadata, "\\d+\\.\\d+"))
-  
+  # print(info)
   # Add columns for the file-specific information
   data$rep <- info[1]
   data$width <- info[2]
@@ -49,18 +49,18 @@ df <- data.frame()
 # Loop over the file names
 for (file_name in file_list) {
   # Extract the parameters from the file name
-  params <- strsplit(file_name, "_")[[1]] %>% extract_numeric() 
+  params <- strsplit(file_name, "_")[[1]] %>% parse_number()
   params <- params[is.na(params)==F]
-  
+  print(params)
   replicate <- as.integer(params[1])
   width <- as.numeric(params[2])
   selection_coefficient <- as.numeric(params[3])
   mutation_rate <- as.numeric(params[4])
   pop_density <- as.numeric(params[5])
   sigma <- as.numeric(params[6])
+  num_samples <- as.integer(params[7])
+  print(num_samples)  
 
-  num_samples <- as.integer(sub("\\.sfs\\.tsv", "", params[7]))
-  
   # Read the file into a data frame
   file_path <- paste(inputdir, file_name,sep = "/")  # Replace with the actual file path
   data <- read.table(file_path, skip = 1 , header= F,na.strings = "NA") 
@@ -85,7 +85,7 @@ for (file_name in file_list) {
 # Assuming your data frame is named 'df'
 df_summary <- df %>%
   pivot_longer(cols=all_of(header[-length(header)]),names_to = "width",values_to = "f") %>%
-  group_by(s,width,`allele counts`) %>% # mean across replicates
+  group_by(s,width,`allele counts`,sampnum) %>% # mean across replicates
   summarise(se_f=sd(f,na.rm=T),f=mean(f,na.rm = T),n=n()) %>%
   mutate(f_per_kb = (f/1e8) * 1e3) %>%
   ungroup()
--- simulations/scripts/verify_env.py
+++
@@ -1,1 +0,0 @@
-import pyslim as ps
