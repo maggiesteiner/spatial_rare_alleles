21ca09cae7fa9ddef64f3399e25c834aba6d8649
Maggie Steiner <steinerm@uchicago.edu>
2024-09-18 08:11:19 -0500

    remove old file paths

--- bp_sims/run_output_moments.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-input_file="files_to_read.txt"
-
-while IFS= read -r line; do
-    modified_line=$(echo "$line" | sed 's/\.[^.]*$//;s/\.$//')
-    python3 output_moments.py -f "$modified_line"
-done < "$input_file"
