900c87cb4e1dc77148b269cc291fcea22489efe7
Maggie Steiner <steinerm@uchicago.edu>
2024-10-10 15:55:51 -0500

    update script for IS

--- empirical/subsampling_vF/scripts/gen_fam_files.py
+++ empirical/subsampling_vF/scripts/gen_fam_files.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import numpy as np
 
+def gaussian_2d(r, sigma):
+    return (1 / (2 * np.pi * sigma ** 2)) * np.exp(-r ** 2 / (2 * sigma ** 2))
+
 df = pd.read_csv(snakemake.input[0])
+df_IS_geo = pd.read_csv(snakemake.input[1])
+df_IS_pca = pd.read_csv(snakemake.input[2])
 seed = int(snakemake.params.seed)
 np.random.seed(seed)
 
 pca_column = "used_in_pca"
 birth_column = "birth_UKelsewhere"
 east_coord_column = "birth_east_coord"
+
+IS_labels = [
+    '5epsilonpcaIS',
+    '10epsilonpcaIS',
+    '50epsilonpcaIS',
+    '100epsilonpcaIS',
+    'uniformpcaIS',
+    '5epsilongeoIS',
+    '10epsilongeoIS',
+    '50epsilongeoIS',
+    'uniformgeoIS'
+]
 
 pca_columns = {
     "closest10kpca": "closest_10k_pca",
@@ -28,6 +45,9 @@ geo_columns = {
 label = snakemake.params.label
 filtered_df = pd.DataFrame()  
 
+epsilon_geo = snakemake.params.eps_geo
+epsilon_pca = snakemake.params.eps_pca
+
 if label == "uniform":
     filtered_df = df[df[pca_column] == 1]  # Filter to 'used_in_pca == 1'
 elif label in pca_columns:
@@ -36,18 +56,51 @@ elif label in geo_columns:
     filtered_df = df[(df[pca_column] == 1) & (df[geo_columns[label]] == True) & (df['within_1epsilon_pca']==True) & (df[east_coord_column].notna()) & (df[birth_column]!='Elsewhere')] 
 elif label == "uniformgeo":
     filtered_df = df[(df[pca_column] == 1) & (df['within_1epsilon_pca']==True) & (df[east_coord_column].notna()) & (df[birth_column]!='Elsewhere')]
+elif label in IS_labels:
+    if label == "uniformpcaIS":
+        df_IS_pca['weights'] = 1/df_IS_pca['pc_density']
+    if label == "5epsilonpcaIS":
+        df_IS_pca['weights'] = df_IS_pca.apply(lambda row: gaussian_2d(row['distance_to_centroid_pca'],5*epsilon_pca), axis=1)/df_IS_pca['pc_density']
+    if label == "10epsilonpcaIS":
+        df_IS_pca['weights'] = df_IS_pca.apply(lambda row: gaussian_2d(row['distance_to_centroid_pca'],10*epsilon_pca), axis=1)/df_IS_pca['pc_density']
+    if label == "50epsilonpcaIS":
+        df_IS_pca['weights'] = df_IS_pca.apply(lambda row: gaussian_2d(row['distance_to_centroid_pca'],50*epsilon_pca), axis=1)/df_IS_pca['pc_density']
+    if label == "100epsilonpcaIS":
+        df_IS_pca['weights'] = df_IS_pca.apply(lambda row: gaussian_2d(row['distance_to_centroid_pca'],100*epsilon_pca), axis=1)/df_IS_pca['pc_density']
+    if label == "uniformgeoIS":
+        df_IS_geo = df_IS_geo[df_IS_geo['within_1epsilon_pca']==True]
+        df_IS_geo['weights'] = 1/df_IS_geo['geo_density']
+    if label == "5epsilongeoIS":
+        df_IS_geo = df_IS_geo[df_IS_geo['within_1epsilon_pca'] == True]
+        df_IS_geo['weights'] = df_IS_geo.apply(
+            lambda row: gaussian_2d(row['distance_to_centroid_geo'], 5 * epsilon_geo), axis=1) / df_IS_geo['geo_density']
+    if label == "10epsilongeoIS":
+        df_IS_geo = df_IS_geo[df_IS_geo['within_1epsilon_pca'] == True]
+        df_IS_geo['weights'] = df_IS_geo.apply(
+            lambda row: gaussian_2d(row['distance_to_centroid_geo'], 10 * epsilon_geo), axis=1) / df_IS_geo['geo_density']
+    if label == "50epsilongeoIS":
+        df_IS_geo = df_IS_geo[df_IS_geo['within_1epsilon_pca'] == True]
+        df_IS_geo['weights'] = df_IS_geo.apply(
+            lambda row: gaussian_2d(row['distance_to_centroid_geo'], 50 * epsilon_geo), axis=1) / df_IS_geo['geo_density']
 else:
     raise ValueError(f"Unexpected label: {label}")
 
-# Check if filtering worked
-if label in pca_columns and not filtered_df[pca_columns[label]].all():
-    raise ValueError(f"Filtering error: Not all entries in {pca_columns[label]} are True.")
+# # Check if filtering worked
+# if label in pca_columns and not filtered_df[pca_columns[label]].all():
+#     raise ValueError(f"Filtering error: Not all entries in {pca_columns[label]} are True.")
+#
+# if label in geo_columns and not filtered_df[geo_columns[label]].all():
+#     raise ValueError(f"Filtering error: Not all entries in {geo_columns[label]} are True.")
 
-if label in geo_columns and not filtered_df[geo_columns[label]].all():
-    raise ValueError(f"Filtering error: Not all entries in {geo_columns[label]} are True.")
+if label not in IS_labels:
+    sampled_df = filtered_df.sample(n=snakemake.params.numsamp)
 
+elif label in IS_labels:
+    if 'geo' in label:
+        sampled_df = df_IS_geo.sample(n=snakemake.params.numsamp, weights='weights')
+    elif 'pca' in label:
+        sampled_df = df_IS_pca.sample(n=snakemake.params.numsamp, weights='weights')
 
-sampled_df = filtered_df.sample(n=snakemake.params.numsamp)
 new_df = pd.DataFrame({"X1": sampled_df["id"], "X2": sampled_df["id"], "Clust": label})
 new_df.to_csv(snakemake.output[0], sep="\t", index=False)
 
